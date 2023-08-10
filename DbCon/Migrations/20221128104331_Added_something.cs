45b17d69082bc8cacc75fd32d8095494050a2de7
Prem151947 <tiwaripremnarayan306@gmail.com>
2023-08-10 17:28:12 +0530

    10082023 Add demo

---
+++ DbCon/Migrations/20221128104331_Added_something.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DbCon.Migrations
+{
+    public partial class Added_something : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TestTables",
+                columns: table => new
+                {
+                    StudentId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StudentName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    StandardId = table.Column<int>(type: "int", nullable: true),
+                    Age = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TestTables", x => x.StudentId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TestTables");
+        }
+    }
+}
---
+++ Entity/TestTable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class TestTable
+    {
+        [Key]
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public int? StandardId { get; set; }
+        public int? Age { get; set; }
+    }
+}
